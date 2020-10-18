147a7d625071a888eefd534189be6b4f3439331f
helloworldwriter <writer@helloworldwriter.com>
2020-10-18 18:51:20 +0300

    Shuffle reviews; add shuffle FAB

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,16 +1,24 @@
 import '@material/mwc-button';
 import '@material/mwc-drawer';
+import '@material/mwc-fab';
 import '@material/mwc-icon-button';
 import '@material/mwc-list/mwc-list-item.js';
 import '@material/mwc-list/mwc-list.js';
-// import '@material/mwc-top-app-bar';
 import '@material/mwc-top-app-bar-fixed';
 
-// import '@material/mwc-tab-bar';
-// import '@material/mwc-tab';
+function shuffleReviews() {
+  // Randomize the order of all quotes, using a Fisherâ€“Yates shuffle
+  // based on http://stackoverflow.com/a/11972692/220039
+  var quotes = document.getElementById('reviewsList');
+  for (var i = quotes.children.length; i >= 0; i--) {
+      quotes.appendChild(quotes.children[Math.random() * i | 0]);
+  }
 
-// import '@material/mwc-list';
-// import '@material/mwc-list/mwc-list-item';
+  // Make sure the first quote is always a (random) favorite quote
+  var favQuotes = document.querySelectorAll('[data-fav]');
+  quotes.insertBefore(favQuotes[Math.random() * favQuotes.length | 0], quotes.firstChild);
+}
+
 
 const drawer = document.getElementsByTagName('mwc-drawer')[0];
 if (drawer) {
@@ -19,3 +27,12 @@ if (drawer) {
     drawer.open = !drawer.open;
   });
 }
+
+// Shuffle reviews when the Shuffle FAB is clicked
+const shuffleFAB = document.getElementById('shuffleFAB');
+shuffleFAB.addEventListener('click', () => {
+  shuffleReviews();
+});
+
+// Shuffle reviews on page load
+shuffleReviews();
