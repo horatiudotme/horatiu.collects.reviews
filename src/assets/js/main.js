3408ac0a787b4b64237bee423b8975a39294ae50
helloworldwriter <writer@helloworldwriter.com>
2020-10-19 19:52:49 +0300

    Add content title and footer

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,28 +1,27 @@
-import '@material/mwc-button';
 import '@material/mwc-drawer';
-import '@material/mwc-fab';
+import '@material/mwc-top-app-bar-fixed';
 import '@material/mwc-icon-button';
-import '@material/mwc-list/mwc-list-item.js';
-import '@material/mwc-list/mwc-list.js';
 import '@material/mwc-tab-bar';
 import '@material/mwc-tab';
-import '@material/mwc-top-app-bar-fixed';
+import '@material/mwc-list/mwc-list.js';
+import '@material/mwc-list/mwc-list-item.js';
+// import '@material/mwc-fab';
 
 const reviewList = document.getElementById('reviewList');
 
+// Randomize the order of all reviews, using a Fisher-Yates shuffle
+// based on http://stackoverflow.com/a/11972692/220039
 function shuffleReviews() {
-  // Randomize the order of all quotes, using a Fisherâ€“Yates shuffle
-  // based on http://stackoverflow.com/a/11972692/220039
   for (var i = reviewList.children.length; i >= 0; i--) {
       reviewList.appendChild(reviewList.children[Math.random() * i | 0]);
   }
 
   // Make sure the first quote is always a (random) favorite quote
-  var favQuotes = document.querySelectorAll('[data-fav]');
+  const favQuotes = document.querySelectorAll('[data-fav]');
   reviewList.insertBefore(favQuotes[Math.random() * favQuotes.length | 0], reviewList.firstChild);
 }
 
-
+// Initialize the drawer
 const drawer = document.getElementsByTagName('mwc-drawer')[0];
 if (drawer) {
   const container = drawer.parentNode;
@@ -31,17 +30,17 @@ if (drawer) {
   });
 }
 
-// Shuffle reviews when the Shuffle FAB is clicked
+// Shuffle reviews when the Shuffle button is clicked
 const shuffleButton = document.getElementById('shuffleButton');
 shuffleButton.addEventListener('click', () => {
   shuffleReviews();
 });
 
+// Filter reviews when tab bars are clicked
 const reviewTypeTabBar = document.getElementById('reviewTypeTabBar');
 reviewTypeTabBar.addEventListener('MDCTabBar:activated', (e) => {
-  reviewList.dataset.filter = e.detail.index;
+  document.body.dataset.filter = e.detail.index;
 });
 
 // Shuffle reviews on page load
 shuffleReviews();
-
